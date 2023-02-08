c42176f5cc48eef111d0e8f0cd6b1323289de1b9
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2023-02-08 19:13:45 +0100

    Initial commit

---
+++ src/main/java/com/akmal/cicdplayground/CicdPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.akmal.cicdplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPlaygroundApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CicdPlaygroundApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/akmal/cicdplayground/CicdPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akmal.cicdplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPlaygroundApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
