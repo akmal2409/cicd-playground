41d504328f36c791f46e6b9415d2ab5bfe1e6932
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2023-02-14 20:56:52 +0100

    Update

--- backend/src/main/java/com/akmal/cicdplayground/CicdPlaygroundApplication.java
+++ backend/src/main/java/com/akmal/cicdplayground/CicdPlaygroundApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CicdPlaygroundApplication {
+public class  CicdPlaygroundApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CicdPlaygroundApplication.class, args);
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  health!: any;
+
+  constructor(
+    private readonly http: HttpClient
+  ) {
+    this.pollHealth();
+  }
+
+  public pollHealth() {
+    this.http.get('http://localhost/api/health')
+      .subscribe(health => {
+        this.health = health;
+      });
+  }
 }
